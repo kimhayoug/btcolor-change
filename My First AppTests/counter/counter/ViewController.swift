48ce35e6c82e79f7772022d6195dd36c9a020583
akzmffls <d7703_14@d7703-14ui-imac.local>
2018-03-19 15:28:20 +0900

    counter

---
+++ My First AppTests/counter/counter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  counter
+//
+//  Created by D7703_14 on 2018. 3. 19..
+//  Copyright © 2018년 Personal Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var zero: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    @IBAction func bt1(_ sender: UIButton) {
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
